df9517e5a2bbb721e5e0776746da86e0104fe690
Ravi Kumar <ravi26067@gmail.com>
2024-03-30 23:51:31 +0530

    remove Even

--- Data Structures/Arrays/removeEven.go
+++ Data Structures/Arrays/removeEven.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
+func removeEven(arr []int) []int {
+	var result []int
 
-func removeEven(arr[] int) []int{
-	var result int
-
-	for _,ele := range arr{
-		if ele%2!=0 {
-			result = append(result,ele)
+	for _, ele := range arr {
+		if ele%2 != 0 {
+			result = append(result, ele)
 		}
 	}
 	return result
 }
 
-func main(){
-	arr:=[]int{1,2,3,4,5,6,7,8}
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	newArr := removeEven(arr)
 	fmt.Println(newArr)
 
